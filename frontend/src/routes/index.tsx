768d40fd00318adea944fd31279ff44c07339a91
Raphael Larsen <raphael.j.larsen@gmail.com>
2026-02-12 17:51:00 -0800

    added home

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Title } from "@solidjs/meta";
+import DbFetchTable from "~/components/DbFetchTable";
+
+export default function Membership() {
+  return (
+    <main>
+      <Title>Home</Title>
+      <h1>Home</h1>
+    </main>
+  );
+}
