5f56de5a3022cac51ad0f402316cdb10b582d894
Raphael <raphael.larsen@proton.me>
2026-02-12 21:41:51 -0500

    naieve updates DML.sql

--- DML.sql
+++ DML.sql
@@ -72,3 +72,36 @@ WHERE `event_id` = @event_id;
 
 DELETE FROM `PhysicalEvents` 
 WHERE `event_id` = @event_id;
+
+-- Full updaters (pk excluded)
+UPDATE `Clubs` 
+SET `name` = @club_name, `date_created` = @date_created, `is_active` = @is_active
+WHERE `club_id` = @club_id;
+
+UPDATE `Addresses`
+SET `country_code` = @country_code, `zip_code` = @zip_code, `address_ln1` = @address_ln1, `address_ln2` = @address_ln2, `city` = @city, `state` = @state
+WHERE `address_id` = @address_id;
+
+UPDATE `People`
+SET `address_id` = @address_id, `email` = @email, `onid` = @onid, `phone_number` = @phone_number, `date_of_birth` = @date_of_birth, `gender_identity` = @gender_identity
+WHERE `person_id` = @person_id;
+
+UPDATE `Roles`
+SET `name` = @role_name
+WHERE `role_id` = @role_id;
+
+UPDATE `Membership`
+SET `club_id` = @club_id, `person_id` = @person_id, `role_id` = @role_id
+WHERE `membership_id` = @membership_id;
+
+UPDATE `Events`
+SET `club_id` = @club_id, `organizer_id` = @organizer, `name` = @event_name, `description` = @description, `time_start` = @time_start, `time_end` = @time_end
+WHERE `event_id` = @event_id;
+
+UPDATE `VirtualEvents`
+SET `url` = @url
+WHERE `event_id` = @event_id;
+
+UPDATE `PhysicalEvents`
+SET `address_id` = @address_id
+WHERE `event_id` = @event_id;
